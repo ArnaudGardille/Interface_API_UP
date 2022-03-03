30f39f9f416bf4b1d02edde9f272402b05ea96c9
ArnaudGardille <arnaud.gardille@gmail.com>
2022-03-03 15:46:14 +0100

    Commentaires

--- interface.py
+++ interface.py
@@ -9,29 +9,32 @@ import sys
 sys.tracebacklimit = 0
 
 
-@st.cache
+@st.cache #met le réslutat de la fonction en mémoire pour des arguments donnés
 def get_clients(headers, url):
     """ 
+    Cette fonction récupère la liste des clients d'un cabinet.
+
     Renvoit une liste de dictionnaires de type:
     {
     "name": "Manet FR61453416687",
     "siren": "453416687"
     }
     """
-    r = requests.get(url, headers=headers)
+    request_url = url + '/enterprise'
+    r = requests.get(request_url, headers=headers) # On effectue la requète
 
-    r.raise_for_status()
-    
-    print("content: ", r.content)
-    print("headers: ", r.headers)
-    print("reason: ", r.reason)
-    print("text: ", r.text)
-    content = json.loads(r.content)
+    r.raise_for_status() # Si l'api a rencontré une erreur, on l'affiche à l'utilisateur
+
+    content = json.loads(r.content) # On convertit la réponse de la requète au format json 
     return content
 
-#@st.cache
+# On ne sauvegarde pas la réponse de la fonction, car les fichiers sont suceptibles de 
+# changer de répertoire en raison des téléchargements.
 def get_files(client, invoice_type, headers, url):
     """
+    Cette fonction récupère la liste des noms de fichier pour un client donné.
+    Les fichiers peuvent être dans 'Transfer' ou 'Archived'
+
     client: le SIREN
     invoice_type: AP ou AR
      
@@ -45,23 +48,26 @@ def get_files(client, invoice_type, headers, url):
     }
     """
     content = []
-    for status in ['Transfer', 'Archived']:
-        request_url = url + "/v1/invoice/" + client + "/" + invoice_type + "/" + status
-        #print('url: ', url)
-        r = requests.get(request_url, headers=headers)
-        #print(r.status_code)
+    for status in ['Transfer', 'Archived']: 
+        request_url = url + "/invoice/" + client + "/" + invoice_type + "/" + status
+
+        r = requests.get(request_url, headers=headers) # On effectue la requète
+
         assert r.status_code == 200
-        content += json.loads(r.content)
-        #print(len(content))
-        
+        content += json.loads(r.content) # On convertit la réponse de la requète au format json 
     return content
 
-@st.cache
+@st.cache # On sauvegarde le résultat pour ne par télécharger plusieurs fois un même fichier 
 def download_file(url, headers):
-    return requests.get(url, headers=headers)
+    """
+    Cette fonction télécharge un fichier pdf en le conidérant comme un fichier binaire.
+    """
+    return requests.get(url, headers=headers) # On effectue la requète
 
 def save_file(binary_pdf, path):
-    #print("type: ", type(binary_pdf))
+    """
+    Si le fichier n'existe pas déjà, cette fonction l'enregiste dans le chemin indiqué.
+    """
     if os.path.exists(path):
         print("Le fichier existe déjà")
     else:
@@ -70,20 +76,23 @@ def save_file(binary_pdf, path):
         saved_file.close()
 
 
-environnement = st.sidebar.radio("Environnement", ['UAT', 'PROD'])
+environnement = st.sidebar.radio("Environnement", ['UAT', 'PROD']) # On choisit entre UAT et PROD
 
+# On met a jour l'url en conséquent
 if environnement == 'UAT':
-    url = "https://jefacture.nomadeskpartner.com/comapi/v1/enterprise"
+    url = "https://jefacture.nomadeskpartner.com/comapi/v1"
 else:
-    url = "https://fichiers.jefacture.com/comapi/"
+    url = "https://fichiers.jefacture.com/comapi/v1"
 
-ApiKeyAuth = st.sidebar.text_input("ApiKeyAuth", value="")
+# On demande les clefs d'identification
+ApiKeyAuth = st.sidebar.text_input("ApiKeyAuth", value="") 
 SoftwareID = st.sidebar.text_input("SoftwareID", value="")
 
+# On let met dans un 'header', afin de les envoyer dans nos requètes
 headers= { 'X-SOFTWARE-ID': SoftwareID, 'X-API-KEY' : ApiKeyAuth}
 
-clients = get_clients(headers, url)
-siren_dict = {}
+clients = get_clients(headers, url) #On récupère les clients du cabinet
+siren_dict = {} # 
 name_dict = {}
 for client in clients:
     name = client['name']
@@ -92,8 +101,7 @@ for client in clients:
     siren_dict[name] = siren
     name_dict[siren] = name
 
-name_list = list(name_dict.values())
-siren_list = list(siren_dict.values())
+siren_list = list(siren_dict.values()) # On récupère la liste des sirens
 
 name_col, siren_col = st.columns(2)
 
